186661bfbbea9bf7b4f1f48eafb360004513437c
0zimushr00m <156819279+0zimushr00m@users.noreply.github.com>
2025-04-10 17:51:57 +0900

    Delete mission_table.sql

--- mission_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE member_mission (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  member_id INT,
-  mission_id INT,
-  status VARCHAR(20),
-  FOREIGN KEY (member_id) REFERENCES member(id),
-  FOREIGN KEY (mission_id) REFERENCES mission(id)
-);
